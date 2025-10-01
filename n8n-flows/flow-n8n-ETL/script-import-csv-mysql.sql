9115d66f435b361f17fed1918880c1d1b15b6f02
Edgar Mercado Garcia <emercadogarcia@gmail.com>
2025-10-01 14:36:40 -0400

    Actualizacion de datos

--- n8n-flows/flow-n8n-ETL/script-import-csv-mysql.sql
+++ n8n-flows/flow-n8n-ETL/script-import-csv-mysql.sql
@@ -1,3 +1,9 @@
+-- importante en la conexion:
+
+Finally, click on the Advanced tab and provide an extra line in the Others text input: OPT_LOCAL_INFILE=1. This is important for uploading data from a local CSV file.
+--- agregar ese texto
+
+
 LOAD DATA LOCAL INFILE 'C:/<PATH TO YOUR FILE>/concerts-2023.csv' 
 INTO TABLE concerts2 
 FIELDS TERMINATED BY ',' 
